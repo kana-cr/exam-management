eca87e141e34bd87be92b2721a516fdcab070488
dgxgefg1 <602646397@qq.com>
2020-11-16 21:56:14 +0800

    vue-module

--- vue-module/src/components/user/personalPages/notice/examRegistration.vue
+++ vue-module/src/components/user/personalPages/notice/examRegistration.vue
@@ -11,21 +11,44 @@
       <el-table-column
         prop="examDescription"
         label="考试名称"
+        align="center"
+        width="200"
       ></el-table-column>
-      <el-table-column prop="examLocation" label="考试地点"></el-table-column>
+      <el-table-column
+        prop="examLocation"
+        label="考试地点"
+        align="center"
+        width="200"
+      ></el-table-column>
       <el-table-column
         prop="examStartTime"
         label="考试开始时间"
+        align="center"
+        width="180"
       ></el-table-column>
       <el-table-column
         prop="examEndTime"
         label="考试结束时间"
+        align="center"
+        width="180"
       ></el-table-column>
       <el-table-column
         prop="examAnnounce"
         label="成绩公布日期"
+        align="center"
+        width="250"
       ></el-table-column>
-      <el-table-column prop="location" label="座位"></el-table-column>
+      <el-table-column
+        prop="location"
+        label="座位"
+        align="center"
+        width="60"
+      ></el-table-column>
+      <el-table-column
+        prop="examLocationId"
+        label="准考证"
+        align="center"
+      ></el-table-column>
     </el-table>
     <el-pagination
       @current-change="handleCurrentChange"
@@ -74,7 +97,9 @@ export default {
   mounted: function () {
     this.getRegistrationList();
     var that = this;
-    setTimeout(function () {}, 300);
+    setTimeout(function () {
+      that.getLocation();
+    }, 300);
   },
   methods: {
     //获取还在报名的考试信息
@@ -153,6 +178,30 @@ export default {
 
     handleCurrentChange: function (currentPage) {
       this.currentPage = currentPage;
+    },
+
+    getLocation: function () {
+      var that = this;
+      axios({
+        headers: { Authorization: this.print.Authorization },
+        method: "get",
+        url:
+          "http://kana.chat:70/examLocation/user?userId=" + this.userId.userId,
+      }).then(function (response) {
+        response.data.data.forEach((item) => {
+          for (var i = 0; i < that.fileList.length; i++) {
+            if (item.userExamEntryId == that.fileList[i].userExamEntryId) {
+              that.$set(that.fileList[i], "location", item.location);
+              that.$set(
+                that.fileList[i],
+                "examLocationId",
+                item.examLocationId
+              );
+            }
+          }
+        });
+        that.loading = false;
+      });
     },
   },
 };
